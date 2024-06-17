a0392476d47493114c5bb61a4b67baf5be02c90e
Alexandre Faria <alexandrefonsecaffaria@gmail.com>
2024-06-16 23:03:34 -0300

    mapping commands structs with callbacks setup

--- main.go
+++ main.go
@@ -1,7 +1,50 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
 
 func main() {
-	fmt.Println("yoyoyo")
+	commands := make(map[string]cliCommand)
+
+	commands["help"] = cliCommand{
+		name:        "help",
+		description: "shows help message",
+		callback:    commandHelp,
+	}
+	commands["exit"] = cliCommand{
+		name:        "exit",
+		description: "exit program",
+		callback:    commandExit,
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("pokedex > ")
+	for scanner.Scan() {
+		input := scanner.Text()
+		if input == "exit" {
+			commands["exit"].callback()
+		} else if input == "help" {
+			commands["help"].callback()
+		}
+		fmt.Print("pokedex > ")
+	}
+
+}
+
+func commandHelp() error {
+	fmt.Println("Command HELP")
+	return nil
+}
+
+func commandExit() error {
+	fmt.Println("Command EXIT")
+	return nil
 }
